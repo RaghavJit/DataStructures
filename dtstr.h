b9fa7e9abef0368da6b40e850431f77a16a7363f
Raghav Jit <raghavjit002@gmail.com>
2023-06-04 13:16:37 +0530

    Compilation successfull

--- dtstr.h
+++ dtstr.h
@@ -1,1 +1,2 @@
 #include "./DtStr/Array.h"
+#include "./DtStr/Stack.h"
--- main.cpp
+++ main.cpp
@@ -6,5 +6,11 @@ int main(){
     arr1.insert(0, 7);
     arr1.display();
 
+    dtstr::Stack sta1(4, 1);
+    sta1.push(1);
+    sta1.push(2);
+    sta1.push(3);
+    sta1.pop();
+    sta1.display();
     return 0;
 }
