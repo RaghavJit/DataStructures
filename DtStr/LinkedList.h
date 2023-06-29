2b0622273807d0f2776cdef7d4b4e741682cb3c1
RaghavJit <raghavjit002@gmail.com>
2023-06-29 13:48:23 +0530

    added circular linked list

--- DtStr/LinkedList.h
+++ DtStr/LinkedList.h
@@ -10,8 +10,6 @@ namespace dtstr{
             node_1<DataType>* next;
 
             node_1(DataType value);
-            node_1(node_1<DataType>* n);
-            node_1<DataType>& operator++(int);
     };
 
     template <typename DataType> class iterSL{
@@ -49,11 +47,11 @@ namespace dtstr{
     template <typename DataType> class SinglyLinkedList{
 
         private:
+            node_1<DataType>* start;
             int length;
             bool verbose;
 
         public:
-            node_1<DataType>* start;
 
             SinglyLinkedList(bool ver=true);
             void insert(int index, DataType value);
@@ -62,12 +60,15 @@ namespace dtstr{
             DataType remove(int index);
             void display();
 
+            void message(bool ver);
+            bool isEmpty();
+
             DataType* operator[](int index);
     };
 
-    template <typename DataType> class DoubleLinkedList : public SinglyLinkedList<DataType>{
+    template <typename DataType> class DoubleLinkedList{
 
-        private:
+        protected:
             node_2<DataType>* start;
             node_2<DataType>* end;
             int length;
@@ -82,8 +83,21 @@ namespace dtstr{
             void display();
             void reverse(); 
 
+            void message(bool ver);
+            bool isEmpty();            
+
             DataType* operator[](int index);
 
+    };
+
+    template <typename DataType> class CircularLinkedList : public DoubleLinkedList<DataType> {
+
+        public:
+            CircularLinkedList(bool ver=true);
+            void insert(int index, DataType value);
+            void replace(int index, DataType value);
+            DataType get(int index);
+            DataType remove(int index);
     };
 }
 
